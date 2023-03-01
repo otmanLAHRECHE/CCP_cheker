9b6fb91a28e82317c511317bd45922b6bb4be64f
Otman LAHRECHE <laotman.47@gmail.com>
2023-03-01 15:22:58 +0100

    updates

--- app.py
+++ app.py
@@ -18,8 +18,21 @@ class App(QtWidgets.QMainWindow):
         self.table_vers = self.findChild(QtWidgets.QTableWidget, "tableWidget")
         self.import_file_vers = self.findChild(QtWidgets.QToolButton, "toolButton")
         self.reset_vers = self.findChild(QtWidgets.QPushButton, "pushButton")
-        self.scan_vers = self.findChild(QtWidgets.QPushButton, "pushButton_2")
+        self.scan = self.findChild(QtWidgets.QPushButton, "pushButton_2")
+        self.total = self.findChild(QtWidgets.QLabel, "label_11")
+
+        self.valide = self.findChild(QtWidgets.QLabel, "label_4")
+        self.faut_compte = self.findChild(QtWidgets.QLabel, "label_8")
+        self.faut_request = self.findChild(QtWidgets.QLabel, "label_9")
+
+        self.affichage_ligne = self.findChild(QtWidgets.QCheckBox, "checkBox")
+        self.affichage_full_name = self.findChild(QtWidgets.QCheckBox, "checkBox_2")
+        self.affichage_ccp = self.findChild(QtWidgets.QCheckBox, "checkBox_5")
+        self.affichage_rip = self.findChild(QtWidgets.QCheckBox, "checkBox_3")
+        self.affichage_valeur = self.findChild(QtWidgets.QCheckBox, "checkBox_4")
+
         
+
 
 
 
