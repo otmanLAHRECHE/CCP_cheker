5a84deb539fe55c75588551a0879866574e484fe
Otman LAHRECHE <laotman.47@gmail.com>
2023-03-01 15:13:22 +0100

    updates main

--- app.py
+++ app.py
@@ -11,4 +11,21 @@ class App(QtWidgets.QMainWindow):
 
         uic.loadUi("ui/main_ui.ui", self)
 
+
+        self.tab = self.findChild(QtWidgets.QTableWidget,"tabWidget")
+
+        self.champ_vers = self.findChild(QtWidgets.QLineEdit, "lineEdit")
+        self.table_vers = self.findChild(QtWidgets.QTableWidget, "tableWidget")
+        self.import_file_vers = self.findChild(QtWidgets.QToolButton, "toolButton")
+        self.reset_vers = self.findChild(QtWidgets.QPushButton, "pushButton")
+        self.scan_vers = self.findChild(QtWidgets.QPushButton, "pushButton_2")
         
+
+
+
+
+
+
+
+
+
