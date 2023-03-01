303e90ab1f35c4e48bc7c8533954b266d45ee217
Otman LAHRECHE <laotman.47@gmail.com>
2023-03-01 09:51:29 +0100

    startup files

---
+++ app.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5 import uic, QtWidgets, QtCore
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QApplication, QGraphicsDropShadowEffect
+
+
+
+class App(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+
+        uic.loadUi("ui/main_ui.ui", self)
---
+++ threads.py
@@ -0,0 +1,21 @@
+
+import time
+import PyQt5
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class ThreadLoadingApp(QThread):
+    _signal = pyqtSignal(int)
+    _signal_result = pyqtSignal(bool)
+
+    def __init__(self):
+        super(ThreadLoadingApp, self).__init__()
+
+    def __del__(self):
+        self.terminate()
+        self.wait()
+
+    def run(self):
+        for i in range(100):
+            self._signal.emit(i)
+            time.sleep(0.05)
+        self._signal_result.emit(True)
